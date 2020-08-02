a6bfbbdfc849d154e2aa405d5db701ae570aa13e
suguz06 <drselmanuguz@gmail.com>
2020-08-02 09:58:56 -0500

    day37

---
+++ src/day37_ArrayList/FrequencyOfChars.java
@@ -0,0 +1,34 @@
+package day37_ArrayList;
+
+import Library.Util;
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ 2. write a program that can find the frequency of character from a String
+                        DO NOT use nested loop
+                        "AABBCCDDEE"  ==> "ABCDE"
+                        output: A2B2C2D2E2
+
+ */
+public class FrequencyOfChars {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list= new ArrayList<>();
+       String str= "AABBCCDDEEF";
+        for(String each: str.split("")) {
+            list.add(each);
+        }
+
+        System.out.println(list);
+String nonDup= Util.removeDup(str);
+
+        for (String each: nonDup.split("")) {
+         int a=   Collections.frequency(list,each);
+            System.out.print(each+a);
+        }
+    }
+}
